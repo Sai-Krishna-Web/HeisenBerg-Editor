b4d3fdf2ce9efec9cfcf9a47e0acc7094c4a46b0
Sai-Krishna-Web <60420794+Sai-Krishna-Web@users.noreply.github.com>
2021-03-25 13:23:52 +0530

    Add files via upload

---
+++ build/precache-manifest.a3171cbbb0415c4d15cc4a8f5184faa9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c796c8b715521459e1fba2846ba778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e18f83ba20cb9866456",
+    "url": "/static/css/main.6c2afef8.chunk.css"
+  },
+  {
+    "revision": "6b97411c973d95a65ab2",
+    "url": "/static/js/2.f0d71974.chunk.js"
+  },
+  {
+    "revision": "00ab031367edebf21d0088f580f98a15",
+    "url": "/static/js/2.f0d71974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e18f83ba20cb9866456",
+    "url": "/static/js/main.3ebaff36.chunk.js"
+  },
+  {
+    "revision": "1cbae6b573baa48ffc9b",
+    "url": "/static/js/runtime-main.e23e1dc7.js"
+  },
+  {
+    "revision": "c010e6e3decb04490fd24a0c631fa85a",
+    "url": "/static/media/DocIcon.c010e6e3.png"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  }
+]);
